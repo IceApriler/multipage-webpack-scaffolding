f87aceebed9a3f008ed2da8c8d6dad070444fb44
qimengjie <qimengjie@163.com>
2022-02-26 20:25:42 +0800

    chore: use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,15 +7,6 @@ module.exports = {
   env: {
     browser: true,
   },
-  extends: 'standard',
-  plugins: ['html'],
-  rules: {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    semi: 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-  },
+  extends: 'prettier',
+  plugins: ['prettier'],
 }
